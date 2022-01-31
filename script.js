42aa7167119a5327e456d60e97af9fb4879b58a4
xaax <85655796+xavitinho@users.noreply.github.com>
2022-01-31 01:11:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let links = {
   facebook: ['facebook', 'https://facebook.com/xavier.com.br', 'fb.me/xavier.com.br', 'logo-facebook'],
   twitter: ['twitter', 'https://twitter.com/xavitinho', '@xavitinho', 'logo-twitter'],
   instagram: ['instagram', 'https://www.instagram.com/abcdefghijlmnopqrstu__z/', '@abcdefghijlmnopqrstu__z', 'logo-instagram'],
+  discord: ['discord', 'https://discord.com/users/410886443964039169', 'xavier 🌈#7692', 'logo-discord'],
   tiktok: ['tiktok', 'https://tiktok.com/@xavitinho', '@xavitinho', 'logo-tiktok'],
   spotify: ['spotify', 'https://open.spotify.com/user/asxvitor', '@asxvitor', 'play-circle'],
   youtube: ['youtube', 'https://youtube.com/xrvitro', false, 'logo-youtube'],
@@ -12,49 +13,51 @@ let links = {
 }
 
 let bg = Math.floor(Math.random() * 6)
-let ebg = document.getElementById('bg')
-ebg.style = `background-image: url("backgrounds/${bg}.jpg")`
 
+let ebg = document.getElementById('bg')
+
+ebg.style = `background-image: url("backgrounds/${bg}.jpg")`
 
 makebuttons()
 
-function makebuttons () {
+function makebuttons() {
   let e = document.getElementById('backbt')
   e.innerHTML = ''
 
-  let buttonslist = document.getElementById('section')
+  let buttonslist = document.getElementById('middlesection')
   buttonslist.innerHTML = ''
 
-for (link of Object.keys(links)) {
+  for (link of Object.keys(links)) {
+    buttonslist.innerHTML +=`
+    <div class="link" id="link_${link}">
+      <button class="linkbutton" id="button_${link}">
+        <ion-icon name="${links[link][3]}"></ion-icon>
+        <strong>${links[link][0]}</strong>
+      </button>
+      <div class ="responsive"
+      onclick="window.open('${links[link][1]}','_blank')"
+      onmouseover="mouseover('${link}')"
+      onmouseout="mouseout('${link}')"> </div>
+    </div>`
+  }
   buttonslist.innerHTML +=
-    `<div class="link" id="link_${link}">
-    <button id="button_${link}"
-     onclick="window.open('${links[link][1]}','_blank')"
-     onmouseover="mouseover('${link}')"
-     onmouseout="mouseout('${link}')">
-     <ion-icon name="${links[link][3]}"></ion-icon>
-     <strong>${links[link][0]}</strong>
-  </button>
-  </div>`
-}
-buttonslist.innerHTML +=
-  `<div class="link">
-  <button class="bibliabutton" onclick="biblia()">
+    `<div class="link">
+  <button class="blackbutton" onclick="biblia()">
      <ion-icon name="book"></ion-icon>
      <strong>A Bíblia Sagrada</strong>
   </button>
   </div>`
 
-colorbuttons()
+  colorbuttons()
 }
 
 function colorbuttons() {
-  let colors = ['#f00c', '#f80c', '#ff0c', '#8f0c', '#0f8c', '#0ffc', '#08fc', '#00fc', '#80fc', '#f0fc', '#f08c', '#f0fc']
+  let colors = ['#f00c', '#f80c', '#ff0c', '#8f0c', '#0f0', '#0f8c', '#0ffc', '#08fc', '#00fc', '#80fc', '#f0fc', '#f08c', '#f00c']
   let i = 1;
   let colorant = colors[0]
   for (link of Object.keys(links)) {
     let e = document.getElementById(`link_${link}`)
-    e.style= `background-image: linear-gradient(${colorant}, ${colors[i]});`
+    e.style = `background-image: linear-gradient(${colorant}, ${colors[i]});`
     colorant = colors[i]
     i++;
   }
@@ -68,7 +71,7 @@ function mouseover(link) {
     e.innerHTML = `<strong>${links[link][1].replace('https://', '')}</strong>`
   }
   e = document.getElementById(`link_${link}`)
-  e.style = "background-color: #000a;"
+  e.style = "background-color: #000c;"
 }
 
 function mouseout(link) {
@@ -81,19 +84,19 @@ function mouseout(link) {
 
 async function biblia() {
   let bibliatxt = 'deu ruim'
-  await fetch('https://xavitinho.github.io/biblia.txt')
+  await fetch('https://xaax.repl.co/biblia.txt')
     .then(response => response.text())
     .then(txtResponse => {
       bibliatxt = txtResponse
     })
 
-  let e = document.getElementById('section')
+  let e = document.getElementById('middlesection')
   e.innerHTML = '<div class="bgbiblia"><biblia id="biblia"></biblia></div>'
 
   e = document.getElementById('biblia')
   e.innerText = bibliatxt
   e.innerHTML += '<br><br><br><br><br><br><br><br>'
-  
+
   e = document.getElementById('backbt')
   e.innerHTML = `
     <button class="backbt">
